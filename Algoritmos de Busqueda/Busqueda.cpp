2f9e1b2d6502180d6d56fe0e14bc98761c47fb7f
Pablo Yamamoto <a01022382@gmail.com>
2020-08-31 20:27:10 -0500

    Algoritmos de Busqueda

--- Algoritmos de Busqueda/Busqueda.cpp
+++ Algoritmos de Busqueda/Busqueda.cpp
@@ -1,58 +1,58 @@
-#include "Busqueda.h"
+// #include "Busqueda.h"
 
 
-template<typename T>
-void Busqueda<T>::DesplegarVector(vector<T> Elementos){
-    for(int i = 0; i < Elementos.size(); i++){
-        cout << Elementos.at(i) << "\t"; 
-    }
-    cout << endl; 
-}
+// template<typename T>
+// void Busqueda<T>::DesplegarVector(vector<T> Elementos){
+//     for(int i = 0; i < Elementos.size(); i++){
+//         cout << Elementos.at(i) << "\t"; 
+//     }
+//     cout << endl; 
+// }
 
 
-template<typename T>
-T Busqueda<T>::BusquedaSecuencial(T val){
-    for(int i = 0; i < Elementos.size(); i++){
-        if(Elementos.at(i) == val){
-            return i; 
-        }
+// template<typename T>
+// T Busqueda<T>::BusquedaSecuencial(T val){
+//     for(int i = 0; i < Elementos.size(); i++){
+//         if(Elementos.at(i) == val){
+//             return i; 
+//         }
                 
-    }
+//     }
 
-}
+// }
 
-template<typename T>
-T Busqueda<T>::BusquedaSecuencialOredenada(T val){
-    sort(Elementos.begin(), Elementos.end()); 
-    for(int i = 0; i<Elementos.size(); i++){
-        if(Elementos.at(i) > val){
-            return -1; 
-            break; 
-        }
-        else{
-            return i; 
-            }
-    }
-}
+// template<typename T>
+// T Busqueda<T>::BusquedaSecuencialOredenada(T val){
+//     sort(Elementos.begin(), Elementos.end()); 
+//     for(int i = 0; i<Elementos.size(); i++){
+//         if(Elementos.at(i) > val){
+//             return -1; 
+//             break; 
+//         }
+//         else{
+//             return i; 
+//             }
+//     }
+// }
 
-template<typename T>
-T Busqueda<T>::BusquedaSecuencialOredenada2(T val){
+// template<typename T>
+// T Busqueda<T>::BusquedaSecuencialOredenada2(T val){
 
-}
+// }
 
 
-template<typename T>
-T Busqueda<T>::BusquedaBinaria(T val, int start, int end){
-    while(start <= end){
-        int mid = (start+end)/2; 
-        if(Elementos.at(mid) == val){
-            return mid; 
-        }
-        else if(val > Elementos.at(mid)){
-            return BusquedaBinaria(val, mid+1, end); 
-        }
-        else{
-            return BusquedaBinaria(val, start, mid-1); 
-        }
-    }
-}
+// template<typename T>
+// T Busqueda<T>::BusquedaBinaria(T val, int start, int end){
+//     while(start <= end){
+//         int mid = (start+end)/2; 
+//         if(Elementos.at(mid) == val){
+//             return mid; 
+//         }
+//         else if(val > Elementos.at(mid)){
+//             return BusquedaBinaria(val, mid+1, end); 
+//         }
+//         else{
+//             return BusquedaBinaria(val, start, mid-1); 
+//         }
+//     }
+// }
--- Algoritmos de Busqueda/Busqueda.h
+++ Algoritmos de Busqueda/Busqueda.h
@@ -13,10 +13,51 @@ class Busqueda{
         Busqueda(){}; 
         ~Busqueda(){}; 
 
-        void DesplegarVector(vector<T> Elementos);
-        T BusquedaSecuencial(T val); 
-        T BusquedaSecuencialOredenada(T val); 
-        T BusquedaSecuencialOredenada2(T val); 
-        T BusquedaBinaria(T val, int start, int end); 
+        void DesplegarVector(vector<T> Elementos){
+            for(int i = 0; i < Elementos.size(); i++){
+                cout << Elementos.at(i) << "\t"; 
+            }
+            cout << endl; 
+        };
+
+        T BusquedaSecuencial(T val){
+            for(int i = 0; i < Elementos.size(); i++){
+                if(Elementos.at(i) == val){
+                return i; 
+                }
+                
+            }
+        }; 
+
+        T BusquedaSecuencialOredenada(T val){
+            sort(Elementos.begin(), Elementos.end()); 
+            for(int i = 0; i<Elementos.size(); i++){
+                if(Elementos.at(i) > val){
+                    return -1; 
+                    break; 
+                }
+                else{
+                    return i; 
+                }
+            }
+        }; 
+
+
+        T BusquedaSecuencialOredenada2(T val){}; 
+
+        T BusquedaBinaria(T val, int start, int end){
+            while(start <= end){
+                int mid = (start+end)/2; 
+                if(Elementos.at(mid) == val){
+                    return mid; 
+                }
+                else if(val > Elementos.at(mid)){
+                    return BusquedaBinaria(val, mid+1, end); 
+                }
+                else{
+                    return BusquedaBinaria(val, start, mid-1); 
+                }
+            }
+        };
 
 }; 
