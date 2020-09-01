b28a87551fc320543636ac9f84ad62f86585b97a
Pablo Yamamoto <a01022382@gmail.com>
2020-09-01 14:36:42 -0500

    Tarea Algoritmos de Busqueda

--- Algoritmos de Busqueda/main.cpp
+++ Algoritmos de Busqueda/main.cpp
@@ -9,41 +9,41 @@ int main(){
     
     DECLARE_TIMING(t1);
     START_TIMING(t1);
-    int answer1 = Ejemplo.BusquedaSecuencial(94244); 
+    int answer1 = Ejemplo.BusquedaSecuencial(7200); 
     STOP_TIMING(t1);
     cout << "==================================================================================================================" << endl; 
     SHOW_TIMING(t1, "Busqueda Secuencial ");
-    cout << "El número 94244 se encontró en la posición: " << answer1 << endl;
+    cout << "El número 7200 se encontró en la posición: " << answer1 << endl;
     cout << "==================================================================================================================" << endl; 
 
     DECLARE_TIMING(t2);
     START_TIMING(t2);
-    int answer2 = Ejemplo.BusquedaSecuencialOredenada(94244); 
+    int answer2 = Ejemplo.BusquedaSecuencialOredenada(7200); 
     STOP_TIMING(t2);
     cout << "==================================================================================================================" << endl; 
     SHOW_TIMING(t2, "Busqueda Secuencial Ordenada ");
-    cout << "El número 94244 se encontró en la posición: " << answer2 << endl;
+    cout << "El número 7200 se encontró en la posición: " << answer2 << endl;
     cout << "==================================================================================================================" << endl; 
 
 
     DECLARE_TIMING(t3);
     START_TIMING(t3);
-    int answer3 = Ejemplo.BusquedaSecuencialOredenada2(94244); 
+    int answer3 = Ejemplo.BusquedaSecuencialOredenada2(7200); 
     STOP_TIMING(t3);
     cout << "==================================================================================================================" << endl; 
     SHOW_TIMING(t3, "Busqueda Secuencial Ordenada 2 ");
-    cout << "El número 94244 se encontró en la posición: " << answer3 << endl;
+    cout << "El número 7200 se encontró en la posición: " << answer3 << endl;
     cout << "==================================================================================================================" << endl; 
 
     Ejemplo.OrdenarArreglo(); 
     
     DECLARE_TIMING(t4);
     START_TIMING(t4);
-    int answer4 =  Ejemplo.BusquedaBinaria(94244); 
+    int answer4 =  Ejemplo.BusquedaBinaria(7200); 
     STOP_TIMING(t4);
     cout << "==================================================================================================================" << endl; 
     SHOW_TIMING(t4, "Busqueda Binaria ");
-    cout << "El número 94244 se encontró en la posición: " << answer4 << endl;
+    cout << "El número 7200 se encontró en la posición: " << answer4 << endl;
     cout << "==================================================================================================================" << endl; 
 
     return 0; 
